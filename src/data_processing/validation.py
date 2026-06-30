39a7c5f81a3bed742a6e1b32b863b0edbc2527a5
SimonJ <you@example.com>
2026-06-30 12:16:44 +0100

    add

--- src/data_processing/validation.py
+++ src/data_processing/validation.py
@@ -6,4 +6,12 @@ Data validation functions.
 # Example function to implement:
 def validate_isbn(isbn):
     """Validate ISBN-13 format."""
+    print(isbn)
+    isbn = str(isbn).strip().replace('-', '')    #.replace(" ", "").replace("-", "")
+#    isbn = isbn.replace('-', '')    #.replace(" ", "").replace("-", "")
+    print(isbn)
+
+    if not isbn.isdigit():
+        return False
+    
     return True
